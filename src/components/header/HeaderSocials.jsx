fe5d1b97834416b48dae232e7e8cfd8fb85f38fa
Siddharth Ramawat <siddharth_ramawat@hotmail.com>
2022-07-12 23:55:32 +0530

    Initial Commit

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+    <a href="https://linkedin.com/in/siddharthramawat" target="_blank"></a>
+    <a href="https://github.com/Siddharth-Ramawat" target="_blank"></a>
+    <a href="https://siddharthramawat.github.io" target="_blank"></a>
+    </div>
+  )
+}
